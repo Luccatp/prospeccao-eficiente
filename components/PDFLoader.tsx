2fb1b2543036cc5df293d677978ac72325eb23aa
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-08-04 15:05:29 -0300

    f

--- components/PDFLoader.tsx
+++ components/PDFLoader.tsx
@@ -5,12 +5,12 @@ import getStripe from "@/lib/stripe";
 import { NextResponse } from "next/server";
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface PDFLoaderProps {}
 
 const PDFLoader: FC<PDFLoaderProps> = ({}) => {
   const { user, isLoaded } = useUser();
-  const isInstagram = navigator.userAgent.includes("Instagram");
   const router = useRouter();
   useEffect(() => {
     fetch("/api/is-customer").then(async (res) => {
@@ -28,19 +28,11 @@ const PDFLoader: FC<PDFLoaderProps> = ({}) => {
   }
   return (
     <div className="flex flex-col h-full flex-1 flex-grow items-center">
-      {!isInstagram ? (
-        <object
-          style={{
-            overflow: "auto",
-            WebkitOverflowScrolling: "touch",
-          }}
-          data="/E-book.pdf#toolbar=0"
-          type="application/pdf"
-          className="w-full max-w-[500px] h-full flex-1 flex-grow overflow-y-scroll"
-        />
-      ) : (
-        <p>Abra o site no eu navegador padrão</p>
-      )}
+      <embed
+        src="/E-book.pdf#toolbar=0"
+        type="application/pdf"
+        className="w-full max-w-[600px] h-full flex-1 flex-grow overflow-y-scroll"
+      />
     </div>
   );
 };
